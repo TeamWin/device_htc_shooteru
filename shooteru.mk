ad67659e276d4635905ef134504bd92cca945e5b
AmeriCanAndroid <americanandroidrom@gmail.com>
2011-11-16 00:58:47 -0700

    Update shooteru.mk

--- shooteru.mk
+++ shooteru.mk
@@ -153,7 +153,7 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 
 # This is a high density device with more memory, so larger vm heaps for it.
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapsize=32m
+    dalvik.vm.heapsize=48m
 
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dexopt-data-only=1
